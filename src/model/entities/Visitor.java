8889e10e3bb95c047be926344535fe97f1f11706
Thiago <thiagohcarvalho81@hotmail.com>
2020-06-04 07:16:40 -0300

    Visitor and Dweller classes

---
+++ src/model/entities/Visitor.java
@@ -0,0 +1,110 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Visitor implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private Date horaEntrada;
+	private Date horaSaida;
+	
+	public Visitor() {
+		
+	}
+	
+	public Visitor(Integer id, String name, Date horaEntrada, Date horaSaida) {
+		this.id = id;
+		this.name = name;
+		this.horaEntrada = horaEntrada;
+		this.horaSaida = horaSaida;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getHoraEntrada() {
+		return horaEntrada;
+	}
+
+	public void setHoraEntrada(Date horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+
+	public Date getHoraSaida() {
+		return horaSaida;
+	}
+
+	public void setHoraSaida(Date horaSaida) {
+		this.horaSaida = horaSaida;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((horaEntrada == null) ? 0 : horaEntrada.hashCode());
+		result = prime * result + ((horaSaida == null) ? 0 : horaSaida.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Visitor other = (Visitor) obj;
+		if (horaEntrada == null) {
+			if (other.horaEntrada != null)
+				return false;
+		} else if (!horaEntrada.equals(other.horaEntrada))
+			return false;
+		if (horaSaida == null) {
+			if (other.horaSaida != null)
+				return false;
+		} else if (!horaSaida.equals(other.horaSaida))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Visitor [id=" + id + ", name=" + name + ", horaEntrada=" + horaEntrada + ", horaSaida=" + horaSaida
+				+ "]";
+	}
+	
+	
+	
+	
+	
+}
